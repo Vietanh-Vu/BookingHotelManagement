ac5c613fbbd23eeae98e5bb2e239ff33bf71a49f
manhnguyen41 <nguyenxuanmanh22102003@gmail.com>
2023-07-02 21:44:42 +0700

    nothing

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import NavBar from '../components/NavBar.jsx'
 import {pages} from './Var.jsx'
 import PageHeader from '../components/PageHeader.jsx'
+import SignIn from './Login/SignIn.jsx'
 import HomeIcon from '@mui/icons-material/Home'
 
 function Home() {
@@ -12,6 +13,7 @@ function Home() {
         subTitle="Home page"
         icon={<HomeIcon fontSize="medium" />}
       />
+      <SignIn />
     </>
   )
 }
