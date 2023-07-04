b7a8b80f037130bc99fc2b512ce1d68d198844c4
ttnamnktp <tthanhnam.nktpglobal@gmail.com>
2023-07-04 23:32:46 +0700

    files seperated

---
+++ database/INSERT DATA/Hotel.sql
@@ -0,0 +1,24 @@
+INSERT INTO Hotel (CategoryId, HotelName, IsActive, Address, HotelImg, Description)
+VALUES
+    ('CA01', 'Grand Luxury Hotel', 1, '123 Main Street, City', '', 'A luxurious hotel offering world-class amenities.'),
+    ('CA02', 'Cozy Boutique Hotel', 1, '456 Elm Street, City', '', 'A charming boutique hotel with a warm and inviting ambiance.'),
+    ('CA03', 'Sandy Beach Resort', 1, '789 Ocean Avenue, City', '', 'A beachfront resort perfect for a relaxing getaway.'),
+    ('CA04', 'Budget Inn', 1, '10 Park Lane, City', '', 'An affordable hotel option for budget-conscious travelers.'),
+    ('CA05', 'Business Central Hotel', 1, '55 Commerce Street, City', '', 'A convenient hotel catering to business travelers.'),
+    ('CA06', 'Quaint Bed and Breakfast', 1, '77 Maple Road, City', '', 'A cozy and comfortable bed and breakfast.'),
+    ('CA07', 'Mountain Lodge Retreat', 1, '22 Pine Trail, City', '', 'A rustic lodge nestled in the scenic mountains.'),
+    ('CA08', 'Historic Heritage Hotel', 1, '99 History Lane, City', '', 'A hotel with rich history and architectural charm.'),
+    ('CA09', 'Family Paradise Resort', 1, '1234 Family Avenue, City', '', 'A family-friendly resort with numerous activities.'),
+    ('CA010', 'Pet-Friendly Haven', 1, '567 Pet Street, City', '', 'A hotel that warmly welcomes guests and their furry friends.'),
+    ('CA011', 'Urban Loft Hotel', 1, '321 Loft Avenue, City', '', 'A trendy hotel offering stylish loft-style accommodations.'),
+    ('CA012', 'Seaside Escape Resort', 1, '888 Sea View Drive, City', '', 'A resort situated by the ocean, perfect for relaxation.'),
+    ('CA013', 'Wellness Retreat Spa Hotel', 1, '555 Serenity Road, City', '', 'A hotel dedicated to providing rejuvenating spa experiences.'),
+    ('CA014', 'Eco-Friendly Lodge', 1, '66 Green Lane, City', '', 'An environmentally conscious hotel in harmony with nature.'),
+    ('CA015', 'Modern City Hotel', 1, '777 Urban Street, City', '', 'A sleek and contemporary hotel in the heart of the city.'),
+    -- Continue with more rows as needed
+	('CA01', 'Azure Heights Hotel', 1, '123 Main Street, City', '', 'A luxurious hotel with breathtaking views.'),
+    ('CA02', 'Radiant Oasis Inn', 1, '456 Sunshine Avenue, City', '', 'An oasis of tranquility in the heart of the city.'),
+    ('CA03', 'Enigma Lodge', 1, '789 Mystery Lane, City', '', 'A place where mystery and comfort meet.'),
+    ('CA04', 'Velvet Sky Retreat', 1, '10 Serenity Road, City', '', 'Indulge in luxury beneath the velvet sky.'),
+    ('CA05', 'Midnight Mirage Resort', 1, '55 Mirage Boulevard, City', '', 'Experience the magic of the midnight oasis.')
+;
---
+++ database/INSERT DATA/HotelCategory.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO Category (CategoryName) VALUES
+    ('Luxury'),
+    ('Boutique'),
+    ('Budget'),
+    ('Resort'),
+    ('Extended Stay'),
+    ('Business'),
+    ('Bed and Breakfast'),
+    ('All-Inclusive Resort'),
+    ('Spa and Wellness'),
+    ('Beachfront'),
+    ('Mountain Lodge'),
+    ('Historic'),
+    ('Family-Friendly'),
+    ('Pet-Friendly'),
+    ('Eco-Friendly');
+;
